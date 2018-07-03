47e535bbc8b9df5a1582020738dc72b13eecab75
BzbozAl3rbx <40595680+BzbozAl3rbx@users.noreply.github.com>
2018-07-03 19:18:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -85,7 +85,7 @@ client.on('message', message => {
 
   if(!b5bzlog) return message.reply("I've detected that this server doesn't have a 5bz-log text channel.");*/
   if (message.mentions.users.size < 1) return message.reply("**الرجاء منشن شخص**");
-  if(!reason) return message.reply ("**اكتبب سبب الكيك**");
+  if(!reason) return message.reply ("**اكتب سبب الكيك**");
   if (!message.guild.member(user)
   .bannable) return message.reply("**لايمكنني طرد شخص اعلى من رتبتي**");
 
@@ -287,47 +287,6 @@ client.on('message', message => {
         message.author.send(`**هذا الرابط لـ5 اشخاص فقط و لمدة 24 ساعة **`)
     }
 });
-var dat = JSON.parse("{}");
-function forEachObject(obj, func) {
-    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
-}
-client.on("ready", () => {
-    var guild;
-    while (!guild)
-        guild = client.guilds.find("name", "!                                                                                                  !")
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            dat[Inv] = Invite.uses;
-        })
-    })
-})
-client.on("guildMemberAdd", (member) => {
-    let channel = member.guild.channels.find('name', 'chat');
-    if (!channel) {
-        console.log("!channel fails");
-        return;
-    }
-    if (member.id == client.user.id) {
-        return;
-    }
-    console.log('made it till here!');
-    var guild;
-    while (!guild)
-        guild = client.guilds.find("name", "!                                                                                                  !")
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            if (dat[Inv])
-                if (dat[Inv] < Invite.uses) {
-                    console.log(3);
-                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
- channel.send(` **تم دعوته من قبل **${Invite.inviter}  `)            
- }
-            dat[Inv] = Invite.uses;
-        })
-    })
-});
 client.on('guildMemberAdd', member => {
        var channel =member.guild.channels.find('name', 'chat')
   return channel.send(`**Welcome To Server Chloè ${member} .
@@ -337,9 +296,7 @@ client.on('guildMemberAdd', member => {
 });
 client.on('message', msg => {
   if (msg.author.bot) return;
-  if (!msg.content.startsWith(prefix)) return;
   let command = msg.content.split(" ")[0];
-  command = command.slice(prefix.length);
   let args = msg.content.split(" ").slice(1);
 
     if(command === "امسح") {
