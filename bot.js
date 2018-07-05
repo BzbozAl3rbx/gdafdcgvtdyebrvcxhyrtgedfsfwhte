05ab22ebe845e012796c8d9527036c701a72f34c
BzbozAl3rbx <40595680+BzbozAl3rbx@users.noreply.github.com>
2018-07-05 22:23:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -472,7 +472,49 @@ client.on("guildMemberAdd", (member) => {
         })
     })
 });
-
+var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
+}
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "اسم سرفرك هنا")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        })
+    })
+})
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.find('name', 'اسم روم اللي برسل فيها');
+    if (!channel) {
+        console.log("!channel fails");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('made it till here!');
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "اسم سرفرك هنا")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+                    console.log(3);
+                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
+            channel.send(`**Welcome To ${member.guild.name} Server Chloè ${member} .**
+               Invited By : ${Invite.invite}`);
+ }
+ 
+            dat[Inv] = Invite.uses;
+        })
+    })
+});
 
 
 
