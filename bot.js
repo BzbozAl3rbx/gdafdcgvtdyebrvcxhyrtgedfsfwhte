8dbca8652ad3eb88f3c3f2c4727618f79e4bb747
BzbozAl3rbx <40595680+BzbozAl3rbx@users.noreply.github.com>
2018-07-05 03:51:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -429,51 +429,6 @@ message.author.sendEmbed(embed)
 
 }
 });
-const Discord = require('discord.js');
-const client = new Discord.Client({ fetchAllMembers: true });
-
-var dat = JSON.parse("{}");
-function forEachObject(obj, func) {
-    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
-}
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    var guild;
-    while (!guild)
-        guild = client.guilds.find("!                                                                       !", "GoodIng Group")
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            dat[Inv] = Invite.uses;
-        })
-    })   
-})
-client.on("guildMemberAdd", (member) => {
-    let channel = member.guild.channels.find('chat', 'ask');
-    if (!channel) {
-        console.log("!channel fails");
-        return;
-    }
-    if (member.id == client.user.id) {
-        return;
-    }
-    console.log('made it till here!');
-    var guild;
-    while (!guild)
-        guild = client.guilds.find("!                                                                       !", "GoodIng Group")
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            if (dat[Inv])
-                if (dat[Inv] < Invite.uses) {
-                    console.log(3);
-                    console.log(`**Invite By : ** ${Invite.inviter}`)
-                    channel.send(`**: تم دعوته من قبل  ** ${Invite.inviter}`)
-                }
-            dat[Inv] = Invite.uses;
-        })
-    })
-})
 const Eris = require("eris");
 var codes = new Eris("NDYzNzA5NDcxODMxNjIxNjMz.Dh77mA.7wrRUoWbhf6wG-OxjR-2p3qet5E");
 var codes_id = "464212538876493824";
