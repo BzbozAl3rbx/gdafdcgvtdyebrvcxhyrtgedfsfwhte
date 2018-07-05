f7c33c1d8742e667588f7fe828fddd90aedf890a
BzbozAl3rbx <40595680+BzbozAl3rbx@users.noreply.github.com>
2018-07-05 22:49:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -429,7 +429,9 @@ message.author.sendEmbed(embed)
 
 }
 });
-var dat = JSON.parse("{}");
+ const Discord = require('discord.js');
+const client = new Discord.Client();
+ var dat = JSON.parse("{}");
 function forEachObject(obj, func) {
     Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
 }
@@ -464,52 +466,11 @@ client.on("guildMemberAdd", (member) => {
                 if (dat[Inv] < Invite.uses) {
                     console.log(3);
                     console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
-            channel.send(`**Welcome To Server Chloè ${member} .**
-            Invited By : ${Invite.invite}`);
- }
- 
-            dat[Inv] = Invite.uses;
-        })
-    })
-});
-var dat = JSON.parse("{}");
-function forEachObject(obj, func) {
-    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
-}
-client.on("ready", () => {
-    var guild;
-    while (!guild)
-        guild = client.guilds.find("name", "!                                                                       !")
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            dat[Inv] = Invite.uses;
-        })
-    })
-})
-client.on("guildMemberAdd", (member) => {
-    let channel = member.guild.channels.find('name', 'chat');
-    if (!channel) {
-        console.log("!channel fails");
-        return;
-    }
-    if (member.id == client.user.id) {
-        return;
-    }
-    console.log('made it till here!');
-    var guild;
-    while (!guild)
-        guild = client.guilds.find("name", "!                                                                       !")
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            if (dat[Inv])
-                if (dat[Inv] < Invite.uses) {
-                    console.log(3);
-                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
-            channel.send(`**Welcome To ${member.guild.name} Server Chloè ${member} .**
-               Invited By : ${Invite.invite}`);
- }
+channel.send(`**Welcome To ${member.guild.name} Server Chloè ${member} .** 
+
+Invited By : ${Invite.inviter}`);
+@here
+                }
  
             dat[Inv] = Invite.uses;
         })
