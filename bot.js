11001b48657f45e76b4dbca3f5fdbf5dfe4a3d34
BzbozAl3rbx <40595680+BzbozAl3rbx@users.noreply.github.com>
2018-07-05 05:42:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -429,48 +429,7 @@ message.author.sendEmbed(embed)
 
 }
 });
-const Eris = require("eris");
-var codes = new Eris("NDYzNzA5NDcxODMxNjIxNjMz.Dh77mA.7wrRUoWbhf6wG-OxjR-2p3qet5E");
-var codes_id = "464212538876493824";
-                    var i = "0";
-                    var x = "0";
-codes.on("voiceChannelJoin", (msg) => {
-    x++;
-    S.M.editChannel(codes_id, { name : "Chloè : " + x + " "});
-});
-codes.on("voiceChannelLeave", (msg) => {
-    x--;
-    S.M.editChannel(codes_id, { name : "Chloè : " + x + " "});
-});
 
-codes.on("messageCreate", (msg) => {
-    if(msg.author.id !== "406348958139678720") return codes.createMessage('__**This Command is only for the bot Owner**__');
-    if(msg.content === "$voice") {
-        let users = msg.channel.guild.members.map(m => m.user.id);
-        let messages = [];
-        messages.push(users);
-        setTimeout(function(){
-        while (i <= messages[0].length - 1) {
-            check = msg.channel.guild.members.get(messages[0][i]);
-        if(!check.voiceState.channelID){
-                i++;
-        }else{
-                x++;
-                i++;
-        }
-}
-    console.log(x);
-    codes.createMessage(msg.channel.id, "Voice Online Members Now Are: **"+x+"** Members!");
-    S.M.editChannel(codes_id, { name : "Chloè : " + x + " "});
-    messages = [];
-}, 1);
-    }
-});
-
-
-  
-
-codes.connect("NDYzNzA5NDcxODMxNjIxNjMz.Dh77mA.7wrRUoWbhf6wG-OxjR-2p3qet5E")
 
 
 
