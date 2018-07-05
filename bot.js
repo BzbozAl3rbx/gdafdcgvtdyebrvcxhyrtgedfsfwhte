24fe18e18f0c8232ef7043cece119fd8da27ffd7
BzbozAl3rbx <40595680+BzbozAl3rbx@users.noreply.github.com>
2018-07-06 02:15:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -466,8 +466,7 @@ client.on("guildMemberAdd", (member) => {
                     console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
 channel.send(`**Welcome To Server Chloè ${member} .** 
 
-Invited By : ${Invite.inviter}`);   
-@here                  
+**Invited By :** ${Invite.inviter}`);                     
  }
  
             dat[Inv] = Invite.uses;
