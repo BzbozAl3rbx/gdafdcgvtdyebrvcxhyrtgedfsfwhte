3f3abe0282ad9dc79b11fe881cca36ca591e5541
BzbozAl3rbx <40595680+BzbozAl3rbx@users.noreply.github.com>
2018-07-07 18:26:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -470,7 +470,7 @@ client.on('voiceStateUpdate', (old, now) => {
   const channel = client.channels.get('465173931779948554');
   const currentSize = channel.guild.members.filter(m => m.voiceChannel).size;
   const size = channel.name.match(/\[\s(\d+)\s\]/);
-  if (!size) return channel.setName(`Voice Online: ${currentSize}`);
+  if (!size) return channel.setName(`Chloè : ${currentSize}`);
   if (currentSize !== size) channel.setName(`Voice Online: ${currentSize}`);
 });
 
