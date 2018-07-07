94b4ab8e9466b24cee2e940feb5445b1b4ef5be2
BzbozAl3rbx <40595680+BzbozAl3rbx@users.noreply.github.com>
2018-07-07 18:15:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -466,7 +466,13 @@ channel.send(`**Welcome To Chloè ${member} .**
         })
     })
 });
-
+client.on('voiceStateUpdate', (old, now) => {
+  const channel = client.channels.get('465173931779948554');
+  const currentSize = channel.guild.members.filter(m => m.voiceChannel).size;
+  const size = channel.name.match(/\[\s(\d+)\s\]/);
+  if (!size) return channel.setName(`Voice Online: ${currentSize}`);
+  if (currentSize !== size) channel.setName(`Voice Online: ${currentSize}`);
+});
 
 
 
