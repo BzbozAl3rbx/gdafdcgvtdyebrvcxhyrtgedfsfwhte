0041a308b594f57985fdef9cd459cab44af9412b
BzbozAl3rbx <40595680+BzbozAl3rbx@users.noreply.github.com>
2018-07-28 12:57:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -398,7 +398,14 @@ client.on('message', message => {
     })
     }
     });
-
+ client.on('message',function(message) {
+    let prefix = "!";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`**# ${args}**`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
+}
+}); 
 
 
 
